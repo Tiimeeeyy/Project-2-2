50673313c24bff55d39c05b1a45472fa276a27ca
Timur <timur.schmidt@student.maastrichtuniversity.nl>
2025-04-16 00:03:51 +0200

    Few changes

--- src/main/java/Util.java
+++ src/main/java/Util.java
@@ -15,6 +15,7 @@ public class Util {
     /**
      * Transforms a List object if staff members into a Multivalued HashMap. This transformation ensures fast lookups of
      * groups of Staff members which have the same role, which can be done by utilizing the role.
+     *
      * @param staffList The staff list object to be transformed.
      * @return A {@link MultiValuedMap} containing all staff members of the original list, grouped by role.
      */
@@ -28,20 +29,28 @@ public class Util {
 
     /**
      * Evaluates the staff level at a given shift, time and for a given role.
+     *
      * @param staffByRole The grouped Map containing staff members grouped by role (see {@link #getStaffByRole(List)}
-     * @param day The day for which we want to evaluate.
-     * @param shift The shift we want to evaluate.
-     * @param role The role of Staff we want to evaluate.
+     * @param day         The day for which we want to evaluate.
+     * @param shift       The shift we want to evaluate.
+     * @param role        The role of Staff we want to evaluate.
      * @return A {@link MultiKeyMap} with the input parameters as key and the staff count as the value.
      */
     public MultiKeyMap<Object, Integer> getStaffLevels(MultiValuedMap<StaffMember.Role, StaffMember> staffByRole, DayOfWeek day, StaffMember.Shift shift, StaffMember.Role role) {
         Collection<StaffMember> staffPerRole = staffByRole.get(role);
         int staffCount = (int) staffPerRole.stream()
                 .filter(staffMember -> staffMember.getRole().equals(role))
-                .filter(staffMember -> staffMember.getShiftOnDay(day).equals(shift))
+                .filter(staffMember -> {
+                    StaffMember.Shift assignedShift = staffMember.getShiftOnDay(day);
+                    return assignedShift != null && assignedShift.equals(shift);
+                })
                 .count();
         MultiKeyMap<Object, Integer> staffLevel = new MultiKeyMap<>();
         staffLevel.put(role, day, shift, staffCount);
         return staffLevel;
     }
+
+
+
+
 }
